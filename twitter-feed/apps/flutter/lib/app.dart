fad66cf20e2082137b3644af9b741849d05756cb
Guillaume Diallo-Mulliez <guillaumedm@bam.tech>
2022-04-28 12:29:08 +0200

    :construction: (App) Display raw feed

---
+++ twitter-feed/apps/flutter/lib/app.dart
@@ -0,0 +1,33 @@
+import 'package:clash_of_techs_twitter_feed/data/feed_json_data_source.dart';
+import 'package:clash_of_techs_twitter_feed/entities/feed/feed.dart';
+import 'package:flutter/material.dart';
+
+class ClashOfTechsApp extends StatelessWidget {
+  const ClashOfTechsApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Clash of Techs - Flutter',
+      home: Scaffold(
+        body: FutureBuilder(
+            future: FeedJsonDataSource.loadFromJson(filename: 'data/feed.json'),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return const CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+              final feed = snapshot.data as Feed;
+              return ListView.separated(
+                itemCount: feed.posts.length,
+                separatorBuilder: (context, index) => const Divider(),
+                itemBuilder: (context, index) => ListTile(
+                  title: Text(feed.posts[index].text),
+                ),
+              );
+            }),
+      ),
+    );
+  }
+}
