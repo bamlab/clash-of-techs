594ee1aebb1a50da5595bad869728b71fe7138e3
Guillaume Diallo-Mulliez <guillaumedm@bam.tech>
2022-04-28 11:47:55 +0200

    :construction: (Author) Add Author entity

---
+++ twitter-feed/apps/flutter/lib/entities/author/author.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'author.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Author _$$_AuthorFromJson(Map<String, dynamic> json) => _$_Author(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      username: json['username'] as String,
+      profileImageUrl: json['profileImageUrl'] as String,
+    );
+
+Map<String, dynamic> _$$_AuthorToJson(_$_Author instance) => <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'username': instance.username,
+      'profileImageUrl': instance.profileImageUrl,
+    };
