ce4f83b838f683e26977c89922fd40a06d356eed
Guillaume Diallo-Mulliez <guillaumedm@bam.tech>
2022-04-28 16:20:48 +0200

    :sparkles: (Post) Display author'name & username

--- twitter-feed/apps/flutter/lib/presentation/widgets/post_tile.dart
+++ twitter-feed/apps/flutter/lib/presentation/widgets/post_tile.dart
@@ -2,6 +2,32 @@ import 'package:clash_of_techs_twitter_feed/domain/entities/post/post.dart';
 import 'package:clash_of_techs_twitter_feed/presentation/widgets/avatar.dart';
 import 'package:flutter/widgets.dart';
 
+class PostHeader extends StatelessWidget {
+  final Post post;
+  const PostHeader({Key? key, required this.post}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(right: 3.0),
+          child: Text(
+            post.author.name,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+        Text(
+          '@${post.author.username}',
+          style: const TextStyle(
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class PostTile extends StatelessWidget {
   final Post post;
   const PostTile({Key? key, required this.post}) : super(key: key);
@@ -15,7 +41,14 @@ class PostTile extends StatelessWidget {
           padding: const EdgeInsets.all(10.0),
           child: Avatar(imageUrl: post.author.profileImageUrl),
         ),
-        Expanded(child: Text(post.text)),
+        Expanded(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            PostHeader(post: post),
+            Text(post.text),
+          ],
+        )),
       ],
     );
   }
