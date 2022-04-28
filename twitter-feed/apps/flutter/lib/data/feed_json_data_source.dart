f4079d47bc994bc7d591b7912abee09101db64dd
Guillaume Diallo-Mulliez <guillaumedm@bam.tech>
2022-04-28 11:50:54 +0200

    :building_construction: (FeedJsonDataSource) Create JsonDataSource for Feed

---
+++ twitter-feed/apps/flutter/lib/data/feed_json_data_source.dart
@@ -0,0 +1,13 @@
+import 'dart:convert';
+
+import 'package:clash_of_techs_twitter_feed/entities/feed/feed.dart';
+import 'package:clash_of_techs_twitter_feed/entities/post/post.dart';
+import 'package:flutter/services.dart' as root_bundle;
+
+class FeedJsonDataSource {
+  static Future<Feed> loadFromJson({required String filename}) async {
+    final jsondata = await root_bundle.rootBundle.loadString(filename);
+    final list = json.decode(jsondata) as List<dynamic>;
+    return Feed(posts: list.map((e) => Post.fromJson(e)).toList());
+  }
+}
