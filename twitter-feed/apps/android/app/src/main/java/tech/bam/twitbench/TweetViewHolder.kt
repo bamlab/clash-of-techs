c4e3eb597273c84245b392436db2e2ef6420853a
Nicolas Haan <nicolash@bam.tech>
2022-05-31 13:01:57 +0200

    [Android] Show date on tweet

--- twitter-feed/apps/android/app/src/main/java/tech/bam/twitbench/TweetViewHolder.kt
+++ twitter-feed/apps/android/app/src/main/java/tech/bam/twitbench/TweetViewHolder.kt
@@ -5,8 +5,16 @@ import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import tech.bam.twitbench.databinding.TweetItemBinding
 import tech.bam.twitbench.model.ResponseItemEntity
+import java.time.Instant
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 
 internal class TweetViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    companion object {
+        var formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("d MMM")
+    }
 
     val binding = TweetItemBinding.bind(view)
 
@@ -15,6 +23,8 @@ internal class TweetViewHolder(view: View): RecyclerView.ViewHolder(view) {
         Picasso.get().load(responseItemEntity.author?.profileImageUrl).into(binding.avatarImage)
         binding.name.text = responseItemEntity.author?.name ?: ""
         binding.twitterId.text = responseItemEntity.author?.username?.let {"@${it}"} ?: ""
+        binding.date.text = formatter.format(LocalDateTime.ofInstant(Instant.parse(responseItemEntity.createdAt),
+            ZoneOffset.UTC))
         binding.tweetText.text = responseItemEntity.text ?: ""
         binding.answersCount.text = responseItemEntity.publicMetrics?.replyCount?.toString() ?: ""
         binding.retweetCount.text = responseItemEntity.publicMetrics?.retweetCount?.toString() ?: ""
